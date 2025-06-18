cdea391b9b6fe2f1ddfa197c7f4b2ad5f191522d
Jonathan Mora <nathanmorax@outlook.com>
2025-06-17 20:59:10 -0600

    feat: Added a Color extension to simplify access to the custom colors defined in the asset catalog.

---
+++ iConnect/Resource/Color/Color+Custom.swift
@@ -0,0 +1,15 @@
+//
+//  Color+Custom.swift
+//  iConnect
+//
+//  Created by Jonathan Mora on 17/06/25.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundSecondary = Color("BackgroundBox")
+    static let background = Color("BackgroundPrimary")
+    static let glassOverlay = Color("glassOverlay")
+}
+
