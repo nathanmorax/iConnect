e524eae3f0daae4640820abf466fb03328d3dc52
Jonathan Mora <nathanmorax@outlook.com>
2025-06-23 22:28:36 -0600

    feat: Adjusted view layout

--- iConnect/Views/Request/Response/ResponseViewer.swift
+++ iConnect/Views/Request/Response/ResponseViewer.swift
@@ -14,48 +14,51 @@ struct ResponseViewer: View {
     @Binding var highlightedResponse: AttributedString
     
     var body: some View {
+        
         VStack {
-            
-            HStack {
-                Text("Status: \(responseStatusCode.map(String.init) ?? "No Status")")
-                    .infoTitle2Style()
-                
-                Spacer()
-                Text("Time: \(responseTime.map { "\($0)ms" } ?? "0ms")")
-                    .infoTitle2Style(foregroundStyle: .greenColor)
-                    .cardStyle(backgroundColor: .green2Color)
-            }
-            
-            Divider()
-            
-            ScrollView {
-                HStack(alignment: .top) {
-                    // Números de línea
-                    VStack(alignment: .trailing, spacing: 4) {
-                        let lines = response.split(separator: "\n", omittingEmptySubsequences: false)
-                        ForEach(0..<lines.count, id: \.self) { index in
-                            Text("\(index + 1)")
-                                .font(.system(.caption, design: .monospaced))
-                                .foregroundColor(.grayColor)
-                        }
-                    }
-                    .padding(.trailing, 8)
-                   
-                    // Contenido con sintaxis resaltada
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text(highlightedResponse)
-                            .font(.system(.caption, design: .monospaced))
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .textSelection(.enabled) // Permite seleccionar el texto
-                    }
+            HeaderView(title: "Response")
 
+            VStack {
+                HStack {
+                    Text("Status: \(responseStatusCode.map(String.init) ?? "No Status")")
+                        .infoTitle2Style()
+                    
                     Spacer()
+                    Text("Time: \(responseTime.map { "\($0)ms" } ?? "0ms")")
+                        .infoTitle2Style(foregroundStyle: .greenColor)
+                        .cardStyle(backgroundColor: .green2Color)
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.top, 12)
+                
+                Divider()
+                
+                ScrollView {
+                    HStack(alignment: .top) {
+                        VStack(alignment: .trailing, spacing: 4) {
+                            let lines = response.split(separator: "\n", omittingEmptySubsequences: false)
+                            ForEach(0..<lines.count, id: \.self) { index in
+                                Text("\(index + 1)")
+                                    .font(.system(.caption, design: .monospaced))
+                                    .foregroundColor(.grayColor)
+                            }
+                        }
+                        .padding(.trailing, 8)
+                       
+                        VStack(alignment: .leading, spacing: 0) {
+                            Text(highlightedResponse)
+                                .font(.system(.caption, design: .monospaced))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .textSelection(.enabled)
+                        }
+
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 12)
+                }
             }
+            .padding(.horizontal, 12)
+            .cardStyle()
         }
-        .padding(.horizontal, 12)
     }
 }
 
